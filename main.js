91651386617e3c689e5adff7da33ba9d102dde5d
Andrius <andriuss0316@gmail.com>
2021-07-22 10:19:17 +0300

    20210722

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 let rezultatas = 8000;
 
-
-
+let data1 = parseInt(document.getElementById('data1').value);
+let data2 = parseInt(document.getElementById('data2').value);
 
 const minus1DOM = document.querySelector(".minus1");
 const minus2DOM = document.querySelector(".minus2");
@@ -20,33 +20,33 @@ const game2DOM = document.querySelector(".game2");
 const input1DOM = document.querySelector(".input1");
 const input2DOM = document.querySelector(".input2");
 
-// function minus1Click() {
-//     rezultatas = rezultatas - input1DOM.innerText;
-//     number1DOM.innerText = rezultatas;
+function minus1Click() {
+    rezultatas = rezultatas - data1;
+    number1DOM.innerText = rezultatas;
 
     // galima buvo rasyti = number1DOM.innerText = --rezultatas
-// }
+}
 
-// function minus2Click() {
-//     rezultatas = rezultatas - input2DOM.innerText;
-//     number2DOM.innerText = rezultatas;
+function minus2Click() {
+    rezultatas = rezultatas - data2;
+    number2DOM.innerText = rezultatas;
 
     // galima buvo rasyti = number2DOM.innerText = --rezultatas
-// }
+}
 
-// function plus1Cick() {
-//     rezultatas = rezultatas + input1DOM.innerText;
-//     number1DOM.innerText = rezultatas;
-
-    // galima buvo rasyti = numberDOM.innerText = ++rezultatas
-// }
-
-// function plus2Cick() {
-//     rezultatas = rezultatas + input2DOM.innerText;
-//     number2DOM.innerText = rezultatas;
+function plus1Cick() {
+    rezultatas = rezultatas + data1;
+    number1DOM.innerText = rezultatas;
 
     // galima buvo rasyti = numberDOM.innerText = ++rezultatas
-// }
+}
+
+function plus2Cick() {
+    rezultatas = rezultatas + data2;
+    number2DOM.innerText = rezultatas;
+
+    // galima buvo rasyti = numberDOM.innerText = ++rezultatas
+}
 
 function game1Click() {
     rezultatas = 8000;
@@ -59,29 +59,6 @@ function game2Click() {
 }
 
 
-function calc() {
-    let data1 = document.getElementById('data1').value;
-    let data2 = document.getElementById('data2').value;
-    let start1 = document.getElementById('start1').value;
-    let start2 = document.getElementById('start2').value;
-
-    let plus1 = document.getElementById('plus1').value;
-    let plus2 = document.getElementById('plus2').value;
-    let minus1 = document.getElementById('minus1').value;
-    let minus2 = document.getElementById('minus2').value;
-
-
-    if (plus1 === '+') {
-        number1DOM = document.getElementById('data1').value = number1DOM + data1;
-    } else if (plus2 === '+') {
-        number2DOM = document.getElementById('data2').value = number2DOM + data2;
-    } else if (minus === '-') {
-        number1DOM = document.getElementById('data1').value = number1DOM + data1;
-    } else if (minus === '-') {
-        number2DOM = document.getElementById('data2').value = number2DOM + data2;
-    }
-}
-
 
 minus1DOM.addEventListener('click', minus1Click);
 minus2DOM.addEventListener('click', minus2Click);
@@ -91,3 +68,29 @@ plus2DOM.addEventListener('click', plus2Cick);
 
 game1DOM.addEventListener('click', game1Click);
 game2DOM.addEventListener('click', game2Click);
+
+
+
+
+// function calc() {
+//     let data1 = document.getElementById('data1').value;
+//     let data2 = document.getElementById('data2').value;
+//     let start1 = document.getElementById('start1').value;
+//     let start2 = document.getElementById('start2').value;
+
+//     let plus1 = document.getElementById('plus1').value;
+//     let plus2 = document.getElementById('plus2').value;
+//     let minus1 = document.getElementById('minus1').value;
+//     let minus2 = document.getElementById('minus2').value;
+
+
+//     if (plus1 === '+') {
+//         number1DOM = document.getElementById('data1').value = number1DOM + data1;
+//     } else if (plus2 === '+') {
+//         number2DOM = document.getElementById('data2').value = number2DOM + data2;
+//     } else if (minus1 === '-') {
+//         number1DOM = document.getElementById('data1').value = number1DOM + data1;
+//     } else if (minus2 === '-') {
+//         number2DOM = document.getElementById('data2').value = number2DOM + data2;
+//     }
+// }
